913d41f6c979dd582caa2d601c77acc61b9ce72a
Munky <simu0001@stud.kea.dk>
2025-02-17 15:13:03 +0100

    index3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-const mycategory = new URLSearchParams(window.location.search).get("category");
-const listcontainer = document.querySelector(".category_list");
+let categoriesContainer = document.querySelector(".category_list");
 
-fetch(`https://dummyjson.com/products?category=${mycategory}`);
+fetch(`https://dummyjson.com/products/category-list`)
+  .then((response) => response.json())
+  .then(showCategories);
+
+function showCategories(categories) {
+  console.log(categories);
+
+  const markup = categories
+    .map(
+      (category) =>
+        `<div class="category_list_container">
+            
+            <a href="produktside.html?category=${category}">${category}</a></div>`
+    )
+
+    .join("");
+
+  console.log("markup er: ", markup);
+
+  categoriesContainer.innerHTML = markup;
+}
