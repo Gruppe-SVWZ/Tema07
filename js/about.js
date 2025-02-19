c7fdc2611618327124a4405724574c2ef997cd0c
William <wihe0001@stud.kea.dk>
2025-02-19 01:08:51 +0100

    opdateret generel css

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
-let categoriesContainer = document.querySelector(".categories li");
+let categoriesContainer = document.querySelector(".categories");
 
 fetch(`https://dummyjson.com/products/categories`)
   .then((response) => response.json())
@@ -17,4 +17,32 @@ function showCategories(categories) {
       console.log("markup er: ", markup);
     
       categoriesContainer.innerHTML = markup;
+    }
+
+
+
+
+
+
+    // category dropdown menu JS
+    let dropdownContainer = document.querySelector(".dropdown_ul");
+
+fetch(`https://dummyjson.com/products/categories`)
+  .then((response) => response.json())
+  .then(showCategoriesDropdown);
+
+function showCategoriesDropdown(categories) {
+  console.log(categories);
+
+  const markup = categories
+    .map(
+      (category) =>
+        `<li><a href="produktside.html?category=${category.slug}">${category.name}</a></li>
+`)
+    
+      .join("");
+
+      console.log("markup er: ", markup);
+    
+      dropdownContainer.innerHTML = markup;
     }
